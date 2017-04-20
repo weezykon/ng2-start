ad5781f2b87d43453d1d8d6a800128bd73231bf3
Weezykon Akinbode <weezykon@gmail.com>
2017-04-20 10:17:18 +0100

    app directory

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: `
+		<div class="jumbotron">
+			<h1>Hi Buddy, Welcome To My Angular 2 QuickStart</h1>
+		</div>
+	`,
+	styles: [`
+		.jumbotron{
+			box-shadow: 0 2px 0 rgba(0,0,0,0.2);
+		}
+	`]
+})
+
+export class AppComponent {}
